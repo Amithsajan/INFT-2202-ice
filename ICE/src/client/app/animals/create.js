66ceafb1e801d666caf3ef45bb5e1b0dd9f52f4d
Amithsajan <amith.pappachensajan@dcmail.ca>
2025-02-20 17:18:56 -0500

    Some files deleted

--- ICE/src/client/app/animals/create.js
+++
@@ -1,7 +0,0 @@
-/*
-    Name: Amith Pappachen Sajan
-    filename: app.js
-    Course: INFT 2202
-    Date: 10th jan 2025
-    Description: This is my general application script.  Functions that are required on every page should live here.
-*/
